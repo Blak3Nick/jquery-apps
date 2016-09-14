bd044284f6775c0d89b52e4b8427dac53cb086b7
Blak3Nick <blaken2021@gmail.com>
2016-09-14 12:02:59 -0400

    created folder structure for drawing application

---
+++ Simple Drawing Application/js/app.js
@@ -0,0 +1,38 @@
+//Problem: No user interaction causes no change to application
+//Solution: When user interacts cause changes appropriately
+var color = $(".selected").css("background-color");
+
+//When clicking on control list items
+$(".controls li").click(function(){
+  //Deselect sibling elements
+  $(this).siblings().removeClass("selected");
+  //Select clicked element
+  $(this).addClass("selected");
+  //cache current color
+  color = $(this).css("background-color");
+});
+
+//When new color is pressed
+$("#revealColorSelect").click(function(){
+  //Show color select or hide the color select
+  changeColor();
+  $("#colorSelect").toggle();
+});
+
+//update the new color span
+function changeColor() {
+  var r = $("#red").val();
+  var g = $("#green").val();
+  var b = $("#blue").val();
+  $("#newColor").css("background-color", "rgb(" + r + "," + g +", " + b + ")");
+}
+
+//When color sliders change
+$("input[type=range]").change(changeColor);
+
+//When add color is pressed
+  //Append the color to the controls ul
+  //Select the new color
+
+//On mouse events on the canvas
+  //Draw lines
