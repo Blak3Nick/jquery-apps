5f3e7cbaf2d514cb6d62bc005d76229b1e7dd9bc
Blak3Nick <blaken2021@gmail.com>
2016-09-14 11:13:53 -0400

    added new logic in app.js

--- Password Confirmation/js/app.js
+++ Password Confirmation/js/app.js
@@ -1,24 +1,29 @@
-//Problem: Hints are shown even when form is valid
-//Solution: Hide and show them at appropriate times
+var $password = $("#password");
+var $confirmpassword = $("#confirm_password");
 
-//Hide hints
+
 $("form span").hide();
+function isPasswordValid {
+  return $password.val().length >8;
+}
 
 function passwordEvent(){
     //Find out if password is valid
-    if($(this).val().length > 8) {
+    if($(isPasswordValid) {
       //Hide hint if valid
-      $(this).next().hide();
+      $password.next().hide();
     } else {
       //else show hint
-      $(this).next().show();
+      $password.next().show();
     }
 }
 
 //When event happens on password input
 $("#password").focus(passwordEvent).keyup(passwordEvent);
-
+function confirmPasswordEvent() {
+  if($password.val)
+}
 //When event happens on confirmation input
   //Find out if password and confirmation match
     //Hide hint if match
-    //else show hint 
+    //else show hint
